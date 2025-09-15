800339536c5dd501b76bcd4d890769bc1f94198c
MoazzamAli <moazzam.may@gmail.com>
2025-09-15 12:14:41 +0500

    Second commit of task 8

--- string_matching.cpp
+++ string_matching.cpp
@@ -27,4 +27,12 @@ int main(){
     cout<<"Test case for substring at the end:"<<endl;
     string sub2 = "ing";
     cout<<match_string(str, sub2)<<endl;
+
+    //Second commit
+    cout<<"Test case for pattern not present:"<<endl;
+    string sub3 = "kit";
+    cout<<match_string(str,sub3)<<endl;
+    string sub4 = "";
+    cout<<"Test case for empty string:"<<endl;
+    cout<<match_string(str, sub4);
 }
